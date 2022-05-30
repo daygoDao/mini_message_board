5aa7ba3da5a00f52d641170371e47eea08dacbcf
dao vo <gary.vo32@gmail.com>
2022-05-30 11:15:09 -0700

    added messages arr to test output via views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amir",
+    add: new Date()
+  },
+  {
+    text: "Hello world!",
+    user: "karl",
+    add: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'mini message board' });
 });
 
 module.exports = router;
