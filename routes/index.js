1b8ab91c2a3837110254496237605e5b66aac93b
dao vo <gary.vo32@gmail.com>
2022-05-31 14:48:03 -0700

    mini messageboard is able to take new messages and redirect back to home /

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'mini message board' , messages: messages});
 });
 
+router.get("/new", function (req, res, next) {
+  res.render("form", { idk: "yo" });
+});
+
+router.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.newText,
+    user: req.body.user,
+    added: new Date(),
+  });
+  // console.log('trying ' + messages)
+  res.redirect('/');
+});
+
 module.exports = router;
+// module.exports = messages;
--- routes/newMessage.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// get /new route
-router.get('/', function(req, res, next) {
-  res.render('form', {idk: 'yo'});
-});
-
-module.exports = router;
